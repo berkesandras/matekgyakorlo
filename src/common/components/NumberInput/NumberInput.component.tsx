1db7d4c2d85260c284d3565b438bac43cee914bd
András Berkes <andras.berkes@kriansoft.com>
2023-04-29 19:16:36 +0200

    Number input implementation

--- src/common/components/NumberInput/NumberInput.component.tsx
+++ src/common/components/NumberInput/NumberInput.component.tsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./NumberInput.module.css";
 
-export default function NumberInput() {
+interface Props {
+    value?: number;
+    onChange?(value: number | undefined): any;
+}
 
-    const [ value, setValue ] = useState<number>();
+export default function NumberInput({
+    value,
+    onChange,
+}: Props): JSX.Element {
+
+    const [ valueInternal, setValueInternal ] = useState<number>();
+
+    useEffect(() => {
+        setValueInternal(value);
+    }, []);
+
+    useEffect(() => {
+        onChange && onChange(valueInternal);
+    }, [ valueInternal ]);
 
     function onKeyUp(e: any) {
-        const currentValue = value || 0;
+        const currentValue = valueInternal || 0;
 
-        if (e.key === "ArrowUp" && currentValue < 9) {
-            setValue(currentValue + 1);
+        if (e.key === "Delete") {
+            setValueInternal(undefined);
+        } else if (e.key === "ArrowUp" && currentValue < 9) {
+            setValueInternal(currentValue + 1);
         } else if (e.key === "ArrowDown" && currentValue > 0) {
-            setValue(currentValue - 1);
+            setValueInternal(currentValue - 1);
         } else {
             const numericValue = parseInt(e.key, 10);
             if (!isNaN(numericValue)) {
-                setValue(numericValue);
+                setValueInternal(numericValue);
             }
         }
     }
 
     return (
         <div className={styles.root} onKeyUp={onKeyUp} tabIndex={0}>
-            {value}
+            {valueInternal}
         </div>
     );
 }
--- src/pages/addition/Addition.page.tsx
+++ src/pages/addition/Addition.page.tsx
@@ -3,6 +3,13 @@ import styles from "./Addition.module.css";
 
 export default function AdditionPage() {
 
+    const digits: number[] = [];
+
+    function updateDigit(position: number, value: number) {
+        digits[position] = value;
+        console.log(parseInt(digits.join(''), 10));
+    }
+
     return (
         <div className={styles.root}>
             <h1>Írásbeli összeadás gyakorlása</h1>
@@ -13,19 +20,24 @@ export default function AdditionPage() {
 
             <div className={styles.operationArea}>
                 <div className={styles.numbers}>
-                    <span>1234</span>
+                    <span>768</span>
                 </div>
 
                 <div className={styles.plusSign}>+</div>
 
                 <div className={styles.numbers}>
-                    <span>22345</span>
+                    <span>56</span>
                 </div>
 
                 <div className={styles.divider}></div>
 
-                <div className={styles.inputs}>
-                    <NumberInput />
+                <div className={styles.numberInputs}>
+                    <NumberInput onChange={(value) => updateDigit(5, value || 0)} />
+                    <NumberInput onChange={(value) => updateDigit(4, value || 0)} />
+                    <NumberInput onChange={(value) => updateDigit(3, value || 0)} />
+                    <NumberInput onChange={(value) => updateDigit(2, value || 0)} />
+                    <NumberInput onChange={(value) => updateDigit(1, value || 0)} />
+                    <NumberInput onChange={(value) => updateDigit(0, value || 0)} />
                 </div>
             </div>
         </div>
