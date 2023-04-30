ef8ecb398aca866e8dd8edb02eb641519ef49cc0
Berkes András <andras.berkes@kriansoft.com>
2023-04-30 07:18:41 +0200

    Keyboard navigation

--- src/common/components/KeyboardNavigationAwareContainer/KeyboardNavigationAwareContainer.component.tsx
+++ src/common/components/KeyboardNavigationAwareContainer/KeyboardNavigationAwareContainer.component.tsx
@@ -1,12 +1,21 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren, useRef } from "react";
 
 export default function KeyboardNavigationAwareContainer({
     children,
 }: PropsWithChildren) {
 
+    const containerRef = useRef(null);
+
+    function handleKeyUp(e: any) {
+        const container = containerRef.current as unknown as HTMLDivElement;
+        for (const child of container.children) {
+            
+        }
+    }
+
     return (
-        <div>
-            {React.Children.map(children, c => c)}
+        <div ref={containerRef} onKeyUp={handleKeyUp}>
+            {children}
         </div>
     );
 }
