90a616cfbaf7f7c9592480f762f08a9dcf1ee9ef
András Berkes <andras.berkes@kriansoft.com>
2023-05-10 09:52:28 +0200

    Instructions on substraction page

--- src/pages/addition/Addition.page.tsx
+++ src/pages/addition/Addition.page.tsx
@@ -3,6 +3,7 @@ import KeyboardNavigationAwareContainer from "../../common/components/KeyboardNa
 import NumberInput from "../../common/components/NumberInput/NumberInput.component";
 import styles from "./Addition.module.css";
 import useAdditionGame from "./hooks/useAdditionGame";
+import NavigationInstruction from "./components/NavigationInstruction/NavigationInstruction";
 
 export default function AdditionPage() {
 
@@ -51,19 +52,7 @@ export default function AdditionPage() {
             </div>
 
             <div className={styles.instructions}>
-                <h2>Irányítás:</h2>
-                <div>
-                    <span className={styles.instructionsSymbol}>↑ ↓</span>: szám növelése/csökkentése
-                </div>
-                <div>
-                    <span className={styles.instructionsText}>0-9</span>: szám beírása
-                </div>
-                <div>
-                    <span className={styles.instructionsSymbol}>← →</span>: helyiérték léptetése
-                </div>
-                <div>
-                    <span className={styles.instructionsText}>Enter</span>: eredmény ellenőrzése
-                </div>
+                <NavigationInstruction />
             </div>
 
             <div className={styles.operationArea}>
